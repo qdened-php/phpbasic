31f65f5fe0381afb7253e05262f8deb963ce4ab4
qdened <qdened@gmail.com>
2015-03-08 11:07:39 +0700

    Bài học đầu tiên - First lesson

---
+++ first.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>Hello world</title>
+<meta name="description" content="Tự học PHP | qDened"/>
+<meta name="author" content="https://github.com/qdened"/>
+<meta name="copyright" content="Copyright (c) 2015"/>
+</head>
+<body>
+<?php
+echo "Hello world"; //Xuất ra màn hình chuỗi "Hello wolrd"
+?>
+</body>
+</html>
